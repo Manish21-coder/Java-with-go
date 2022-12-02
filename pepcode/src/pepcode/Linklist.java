d3b2785423ffe305292d6abcb45023d6a84842ec
manish kumar raghav <raghavmanishprakash@gmail.com>
2022-12-03 00:57:20 +0530

    new programs

---
+++ pepcode/src/pepcode/Linklist.java
@@ -0,0 +1,147 @@
+package pepcode;
+
+public class Linklist {
+
+	Node head;
+	private int size;
+	
+	// we create new class for node to store next node space.
+
+	// parent class constructor
+	Linklist(){
+		this.size=0;
+	}
+	class Node {
+
+		String data;
+		Node next;
+
+		public Node(String data) {
+			this.data = data;
+			this.next = null;
+			size++;
+		}
+	}
+
+	// INSERTS or ADD method's.
+	// Node first,last insert medthod's
+
+	public void addFirst(String data) {
+
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+
+		newNode.next = head;
+		head = newNode;
+
+	}
+
+	// add at last positon.
+
+	public void addLast(String data) {
+
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+
+		Node curnode = head;
+		while (curnode.next != null) {
+			curnode = curnode.next;
+		}
+
+		curnode.next = newNode;
+	}
+
+	// print
+	public void printlist() {
+		if (head == null) {
+			System.out.println("empty linkedlist");
+			return;
+		}
+
+		Node curnode = head;
+
+		while (curnode != null) {
+			System.out.print(curnode.data + "->");
+			curnode = curnode.next;
+		}
+
+		System.out.println("Null");
+	}
+
+	
+	
+	// deletion of linklist node
+	// in deletion there is also both first and last delete
+	
+	public void deleteFirst() {
+		if(head == null) {
+			System.out.println("list is empty");
+		    return;
+		}
+		
+		size--;
+		head = head.next; // just shifting the head and java grabage collecter automatically delete it for free
+		
+		
+	}
+	
+	// last delete
+	
+	public void deleteLast() {
+		
+		// corner case
+		if(head==null) {
+			System.out.println("List is empty");
+			return;
+			
+		}
+		
+		size--;
+		
+		if(head.next==null) {
+			head=null;
+			return;
+		}
+		
+		Node secondLast = head;
+		Node lastNode = head.next;
+		while(lastNode.next !=null) {
+			lastNode =lastNode.next;
+			secondLast= secondLast.next;
+		}
+		secondLast.next=null;
+	}
+	
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public static void main(String args[]) {
+
+		// create a class object for accessing the nodes
+
+		Linklist list = new Linklist();
+		list.addFirst("a");
+		list.addFirst("b");
+		list.printlist();
+
+		list.addLast("family");
+		list.printlist();
+		
+		list.deleteFirst();
+		list.printlist();
+		
+		//list.deleteLast();
+		list.printlist();
+//		
+       System.out.println(list.getSize());
+	}
+}
---
+++ pepcode/src/pepcode/frameworklinkList.java
@@ -0,0 +1,30 @@
+package pepcode;
+
+import java.util.*;
+
+public class frameworklinkList {
+
+	
+	// fuction declatrtion
+	
+	public static void main(String ar[]) {
+		//syntax of linklist  declaration is 
+		LinkedList<String> list = new LinkedList<String>();
+		list.addFirst("a");
+		list.addFirst("is");
+		list.addFirst("this");
+		list.add("list");
+		
+		System.out.println(list);
+		System.out.println(list.size());
+		// remove function of linkedlist.
+		
+		// when no removefist is used simple first is used then it will remove the last node;
+		list.removeFirst();
+		for(int i=0;i<list.size();i++) {
+			
+			System.out.print(list.get(i)+" ->");
+		}
+		System.out.println("null");
+	}
+}
